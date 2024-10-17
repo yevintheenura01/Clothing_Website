70e0b6ebf9793652b02be04ebbeb93041155951f
tharaka <ltharaka96@gmail.com>
2024-10-17 12:25:32 +0530

    aaa

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -67,6 +67,18 @@ const Hero = () => {
                                 Click now
                             </button>
                         </div>
+
+                        <div>
+                            <button className="bg-gradient-to-r from-primary to -secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+                                Click now
+                            </button>
+                        </div>
+
+                        <div>
+                            <button className="bg-gradient-to-r from-primary to -secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+                                Click now
+                            </button>
+                        </div>
                 </div>
             </div>
         </div>
